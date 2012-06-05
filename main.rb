535db29ab421962dcb3aeb96948c15c71e491ada
swzhou <swing.zhou@hotmail.com>
2012-06-05 23:06:11 +0800

    delete a task

--- main.rb
+++ main.rb
@@ -12,6 +12,11 @@ post '/' do
   redirect to('/')
 end
 
+delete '/task/:id' do
+  Task.get(params[:id]).destroy
+  redirect to('/')
+end
+
 get '/:task' do
   @task = params[:task].split('-').join(' ').capitalize
   slim :task
